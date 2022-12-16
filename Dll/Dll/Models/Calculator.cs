02024745e2adf79cd9f1d0c40c09221cdfb3b1ac
Ali200119 <aliit@code.edu.az>
2022-12-16 13:46:35 +0400

    feat: Task 1

---
+++ Dll/Dll/Models/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dll.Models
+{
+    public class Calculator
+    {
+        public void Calculation(int number1, int number2, string process)
+        {
+            int result;
+
+            switch (process)
+            {
+                case "+":
+                    result = number1 + number2;
+                    Console.WriteLine($"Your answer is: {result}");
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    Console.WriteLine($"Your answer is: {result}");
+                    break;
+
+                case "/":
+                    result = number1 / number2;
+                    Console.WriteLine($"Your answer is: {result}");
+                    break;
+
+                case "*":
+                    result = number1 * number2;
+                    Console.WriteLine($"Your answer is: {result}");
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter correct operator");
+                    break;
+            }
+        }
+    }
+}
---
+++ Dll/Dll/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dll.Models;
+
+#region Task 1
+
+//Calculator adinda class yaradirsiz, hemin classin icinde Calculation metodunuz olur. Method 2 parametr qebul edir. 4 emeliyyati (+,-,*,/)hesablayan calculator olacaq. 
+
+Calculator calculator = new Calculator();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("-----Welcome-----");
+
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Please enter first number:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter second number:");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What you want to do? Enter +, -, / or *");
+string process = Console.ReadLine();
+
+calculator.Calculation(number1, number2, process);
+
+#endregion
